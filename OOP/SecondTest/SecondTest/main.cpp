80db39b5e26e906b538d0b289825cb845b8c961a
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-09-22 02:35:40 +0900

    test2

---
+++ OOP/SecondTest/SecondTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SecondTest
+//
+//  Created by 유진원 on 9/22/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!Second\n";
+    return 0;
+}
