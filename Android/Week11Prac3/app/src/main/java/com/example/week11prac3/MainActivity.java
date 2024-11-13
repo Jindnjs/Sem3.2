209810899f09994dfae403ca24b889e6406e876b
Jindnjs <145753929+Jindnjs@users.noreply.github.com>
2024-11-13 11:42:56 +0900

    AND W11P3 v1find

--- Android/Week11Prac3/app/src/main/java/com/example/week11prac3/MainActivity.java
+++ Android/Week11Prac3/app/src/main/java/com/example/week11prac3/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.week11prac3;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,55 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        final EditText edtNum1 = (EditText) findViewById(R.id.edtNum1);
+        final EditText edtNum2 = (EditText) findViewById(R.id.edtNum2);
+        final RadioGroup rdoGroup = (RadioGroup) findViewById(R.id.rdoGroup);
+
+        Button btnNewActivity = (Button) findViewById(R.id.btnNewActivity);
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),
+                        SecondActivity.class);
+
+                int checkedRadioButtonId = rdoGroup.getCheckedRadioButtonId();
+                if (checkedRadioButtonId == R.id.rdoAdd) {
+                    intent.putExtra("operate", "+");
+                }
+                else if (checkedRadioButtonId == R.id.rdoSub) {
+                    intent.putExtra("operate", "-");
+                }
+                else if (checkedRadioButtonId == R.id.rdoMul) {
+                    intent.putExtra("operate", "*");
+                }
+                else if (checkedRadioButtonId == R.id.rdoDiv) {
+                    intent.putExtra("operate", "/");
+                }
+
+                intent.putExtra("Num1",
+                        Integer.parseInt(edtNum1.getText().toString()));
+                intent.putExtra("Num2",
+                        Integer.parseInt(edtNum2.getText().toString()));
+
+                startActivityForResult(intent, 0);
+            }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == RESULT_OK) {
+            int hap = data.getIntExtra("Res", 0);
+            Toast.makeText(getApplicationContext(), "결과:" + hap,
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
